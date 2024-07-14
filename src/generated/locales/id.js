8e0d274889d92118df1828dbb1d034df96fff58a
Rendy Pratama <rendypratama752@gmail.com>
2024-07-14 18:11:47 +0700

    UPDATE: localization

--- src/generated/locales/id.js
+++ src/generated/locales/id.js
@@ -1,31 +1,52 @@
-// Do not modify this file by hand!
-// Re-generate this file by running lit-localize
 
-export const templates = {
-  s020fa68373e629a7: `Profil Perusahaan Story App`,
-  s1354151b102636dc: `Ikuti cerita terbaru.`,
-  s1594bf1b97607f2b: `Kolom Nama harus diisi`,
-  s1f30f2eaf1e38fb9: `Tambah Cerita`,
-  s306974db215e1942: `Buat Cerita Anda`,
-  s3c8b1782bb615bb1: `Nilai-Nilai Kami`,
-  s3f80bb1225cafc5b: `Keragaman dan Inklusi`,
-  s404550cd396ebffd: `Jelajahi Cerita`,
-  s477b606f47008777: `Memberikan platform yang dapat diakses bagi para pencerita untuk berbagi karya mereka dan bagi pembaca untuk menemukan cerita baru yang menginspirasi, menghibur, dan memberikan informasi.`,
-  s4a0f1024d608d7ef: `Integritas dan Hormat`,
-  s53f840e351140536: `Kolom Foto harus diisi`,
-  s63d894b1ddb06289: `Deksripsi`,
-  s77f1479aaab576c4: `Story App adalah platform yang didedikasikan untuk menghubungkan para pencerita dan pembaca. Kami percaya pada kekuatan cerita untuk menghubungkan orang, menginspirasi perubahan, dan membawa kebahagiaan.`,
-  s79588ab7cdf00cf5: `Kreativitas dan Inovasi`,
-  s7fee95b66225e1fb: `Dasbor`,
-  s808e80e4501f33ff: `Visi Kami`,
-  s9f9312ed955b23cb: `Menjadi platform penceritaan terkemuka di dunia, yang memupuk komunitas global para pencerita dan pembaca.`,
-  sb061ff5a347a296e: `Profil`,
-  scb056ed6dbbd6244: `Ikuti Kami`,
-  sd1cae0dcd2945c35: `Hubungi Kami`,
-  se8f5ec44c4c6a98b: `Misi Kami`,
-  se93cb0c892820b0a: `Tentang Kami`,
-  see84ee5332435f6d: `Komunitas dan Koneksi`,
-  sef49aec68fd1dc66: `Nama`,
-  sf1bc83d2509bafcf: `Telepon`,
-  sfabad57e72fa98b4: `Kolom Deskripsi harus diisi`,
-};
+    // Do not modify this file by hand!
+    // Re-generate this file by running lit-localize
+
+    
+    
+
+    /* eslint-disable no-irregular-whitespace */
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+
+    export const templates = {
+      's020fa68373e629a7': `Profil Perusahaan Story App`,
+'s1354151b102636dc': `Ikuti cerita terbaru.`,
+'s1594bf1b97607f2b': `Kolom Nama harus diisi`,
+'s1f30f2eaf1e38fb9': `Tambah Cerita`,
+'s205b3e04ad286caa': `Sudah punya akun?`,
+'s2c15b9b13a3e2a80': `Daftar`,
+'s306974db215e1942': `Buat Cerita Anda`,
+'s3c8b1782bb615bb1': `Nilai-Nilai Kami`,
+'s3f80bb1225cafc5b': `Keragaman dan Inklusi`,
+'s404550cd396ebffd': `Jelajahi Cerita`,
+'s477b606f47008777': `Memberikan platform yang dapat diakses bagi para pencerita untuk berbagi karya mereka dan bagi pembaca untuk menemukan cerita baru yang menginspirasi, menghibur, dan memberikan informasi.`,
+'s4a0f1024d608d7ef': `Integritas dan Hormat`,
+'s53f840e351140536': `Kolom Foto harus diisi`,
+'s63d894b1ddb06289': `Deksripsi`,
+'s67749057edb2586b': `Keluar`,
+'s736ea3fbaecab734': `Kolom Email harus berupa email yang valid`,
+'s77f1479aaab576c4': `Story App adalah platform yang didedikasikan untuk menghubungkan para pencerita dan pembaca. Kami percaya pada kekuatan cerita untuk menghubungkan orang, menginspirasi perubahan, dan membawa kebahagiaan.`,
+'s79588ab7cdf00cf5': `Kreativitas dan Inovasi`,
+'s7b4ea7ef33e1c394': ` Masuk`,
+'s7fee95b66225e1fb': `Dasbor`,
+'s808e80e4501f33ff': `Visi Kami`,
+'s8eb24a0b585e1a03': ` Daftar`,
+'s9547046b6b89f296': `Silakan masuk untuk melihat cerita`,
+'s9f9312ed955b23cb': `Menjadi platform penceritaan terkemuka di dunia, yang memupuk komunitas global para pencerita dan pembaca.`,
+'sab64f55b644f4028': `Tidak ada cerita yang terseedia`,
+'sb061ff5a347a296e': `Profil`,
+'sc8da3cc71de63832': `Masuk`,
+'scad07cc65ad2fb30': `Belum punya akun?`,
+'scb056ed6dbbd6244': `Ikuti Kami`,
+'sd1cae0dcd2945c35': `Hubungi Kami`,
+'sd1f44f1a8bc20e67': `Email`,
+'sd821a681d42add6a': `Kolom Kata Sandi harus minimal 8 karakter`,
+'se8f5ec44c4c6a98b': `Misi Kami`,
+'se93cb0c892820b0a': `Tentang Kami`,
+'see84ee5332435f6d': `Komunitas dan Koneksi`,
+'sef49aec68fd1dc66': `Nama`,
+'sf1bc83d2509bafcf': `Telepon`,
+'sf6e1665c7022a1f8': `Kata Sandi`,
+'sfabad57e72fa98b4': `Kolom Deskripsi harus diisi`,
+    };
+  
--- src/generated/locales/zh.js
+++ src/generated/locales/zh.js
@@ -1,31 +1,52 @@
-// Do not modify this file by hand!
-// Re-generate this file by running lit-localize
 
-export const templates = {
-  s020fa68373e629a7: `Story App 公司简介`,
-  s1354151b102636dc: `了解最新故事。`,
-  s1594bf1b97607f2b: `姓名字段是必填的`,
-  s1f30f2eaf1e38fb9: `添加故事`,
-  s306974db215e1942: `添加您的故事`,
-  s3c8b1782bb615bb1: `我们的价值观`,
-  s3f80bb1225cafc5b: `多样性和包容性`,
-  s404550cd396ebffd: `探索故事`,
-  s477b606f47008777: `为讲故事者提供一个可访问的平台来分享他们的作品，为读者发现新的故事，这些故事可以激励、娱乐 和提供信息。`,
-  s4a0f1024d608d7ef: `诚信和尊重`,
-  s53f840e351140536: `照片字段是必填的`,
-  s63d894b1ddb06289: `描述`,
-  s77f1479aaab576c4: `Story App 是一个致力于将讲故事者和读者聚集在一起的平台。我们相信故事的力量可以连接人们，激励变革，带来快乐。`,
-  s79588ab7cdf00cf5: `创造力和创新`,
-  s7fee95b66225e1fb: `仪表板`,
-  s808e80e4501f33ff: `我们的愿景`,
-  s9f9312ed955b23cb: `成为全球领先的讲故事平台，培育一个全球的讲故事者和读者社区。`,
-  sb061ff5a347a296e: `个人资料`,
-  scb056ed6dbbd6244: `关注我们`,
-  sd1cae0dcd2945c35: `联系我们`,
-  se8f5ec44c4c6a98b: `我们的使命`,
-  se93cb0c892820b0a: `关于我们`,
-  see84ee5332435f6d: `社区和联系`,
-  sef49aec68fd1dc66: `姓名`,
-  sf1bc83d2509bafcf: `电话`,
-  sfabad57e72fa98b4: `描述字段是必填的`,
-};
+    // Do not modify this file by hand!
+    // Re-generate this file by running lit-localize
+
+    
+    
+
+    /* eslint-disable no-irregular-whitespace */
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+
+    export const templates = {
+      's020fa68373e629a7': `Story App 公司简介`,
+'s1354151b102636dc': `了解最新故事。`,
+'s1594bf1b97607f2b': `姓名字段是必填的`,
+'s1f30f2eaf1e38fb9': `添加故事`,
+'s205b3e04ad286caa': `已经有帐户？`,
+'s2c15b9b13a3e2a80': `注册`,
+'s306974db215e1942': `添加您的故事`,
+'s3c8b1782bb615bb1': `我们的价值观`,
+'s3f80bb1225cafc5b': `多样性和包容性`,
+'s404550cd396ebffd': `探索故事`,
+'s477b606f47008777': `为讲故事者提供一个可访问的平台来分享他们的作品，为读者发现新的故事，这些故事可以激励、娱乐 和提供信息。`,
+'s4a0f1024d608d7ef': `诚信和尊重`,
+'s53f840e351140536': `照片字段是必填的`,
+'s63d894b1ddb06289': `描述`,
+'s67749057edb2586b': `退出`,
+'s736ea3fbaecab734': `电子邮件字段必须是有效的电子邮件`,
+'s77f1479aaab576c4': `Story App 是一个致力于将讲故事者和读者聚集在一起的平台。我们相信故事的力量可以连接人们，激励变革，带来快乐。`,
+'s79588ab7cdf00cf5': `创造力和创新`,
+'s7b4ea7ef33e1c394': ` 登录`,
+'s7fee95b66225e1fb': `仪表板`,
+'s808e80e4501f33ff': `我们的愿景`,
+'s8eb24a0b585e1a03': ` 注册`,
+'s9547046b6b89f296': `请登录以查看故事`,
+'s9f9312ed955b23cb': `成为全球领先的讲故事平台，培育一个全球的讲故事者和读者社区。`,
+'sab64f55b644f4028': `没有可用的故事`,
+'sb061ff5a347a296e': `个人资料`,
+'sc8da3cc71de63832': `登录`,
+'scad07cc65ad2fb30': `没有帐户？`,
+'scb056ed6dbbd6244': `关注我们`,
+'sd1cae0dcd2945c35': `联系我们`,
+'sd1f44f1a8bc20e67': `电子邮件`,
+'sd821a681d42add6a': `密码字段必须至少为 8 个字符`,
+'se8f5ec44c4c6a98b': `我们的使命`,
+'se93cb0c892820b0a': `关于我们`,
+'see84ee5332435f6d': `社区和联系`,
+'sef49aec68fd1dc66': `姓名`,
+'sf1bc83d2509bafcf': `电话`,
+'sf6e1665c7022a1f8': `密码`,
+'sfabad57e72fa98b4': `描述字段是必填的`,
+    };
+  
